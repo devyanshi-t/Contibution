16bfc56b9de784fbdc50eae66edf52eb9c05836e
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2020-06-19 16:55:34 +0530

    new changes

---
+++ Codes/loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 16:53:29 2020
+
+@author: devyanshitiwari
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(n):
+    print(i*i)
